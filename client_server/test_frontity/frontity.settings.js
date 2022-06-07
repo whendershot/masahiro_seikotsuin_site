c1852e46175379d5718c86d2038f6a7384a9393d
William Hendershot <whendershot1@gmail.com>
2022-06-07 15:04:24 +0900

    Initital install and Wordpress server connection

---
+++ client_server/test_frontity/frontity.settings.js
@@ -0,0 +1,57 @@
+const settings = {
+  "name": "test-frontity",
+  "state": {
+    "frontity": {
+      "url": "http://localhost:3000",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/mars-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "Nature",
+              "/category/nature/"
+            ],
+            [
+              "Travel",
+              "/category/travel/"
+            ],
+            [
+              "Japan",
+              "/tag/japan/"
+            ],
+            [
+              "About Us",
+              "/about-us/"
+            ]
+          ],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "url": "http://wordpress/"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
