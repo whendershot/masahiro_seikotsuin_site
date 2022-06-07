337797fd15d22c85f91b7cb28596ed0fe0b75f00
William Hendershot <whendershot1@gmail.com>
2022-06-07 16:37:23 +0900

    Checkpoint

---
+++ client_server/test_frontity/packages/my-test-theme/src/index.js
@@ -0,0 +1,17 @@
+import Root from "./components"
+
+const myTestTheme = {
+  name: "my-test-theme",
+  roots: {
+    theme: Root
+  },
+  state: {
+    theme: {}
+  },
+  actions: {
+    theme: {}
+  }
+
+}
+
+export default myTestTheme
