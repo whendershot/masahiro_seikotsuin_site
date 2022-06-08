bb8838ab43b37d7affae0e9686b8f43c2ca5d994
William Hendershot <whendershot1@gmail.com>
2022-06-08 16:40:35 +0900

    CSS as JS Styiling

--- client_server/frontity_test/packages/my-test-theme/src/components/index.js
+++ client_server/frontity_test/packages/my-test-theme/src/components/index.js
@@ -1,36 +1,92 @@
 import React from "react"
-import { connect } from "frontity"
+import { connect, Global, css, styled} from "frontity"
 import Link from "@frontity/components/link"
 import Switch from "@frontity/components/switch"
 
 import List from "./list.view"
 import Post from "./post.view"
+import Page from "./page.view"
 
 const Root = ({state}) => {
     const data = state.source.get(state.router.link)
 
     return (
         <>
-            <h1>Hello World, from Frontity/React!</h1>
-            <p>Now as a component!</p>
-            <p>Current URL: {state.router.link}</p>
-            <nav>
-                <Link link="/">Home</Link>
-                <br/>
-                <Link link="/page/2">More posts</Link>
-                <br/>
-                <Link link="/about-us">About us</Link>
-            </nav>
+            <Global 
+                styles={css`
+                    * {
+                        margin: 0;
+                        padding: 0;
+                        box-sizing: border-box;
+                    }
+                    
+                    html {
+                        font-family: system-ui, Verdana, Arial, sans-serif;
+                    }
+                `}
+            />
+            <Header>
+                <HeaderContent>
+                    <h1>Hello World, from Frontity/React!</h1>
+                    <p>Now as a component!</p>
+                    <p>Current URL: {state.router.link}</p>
+                    <nav>
+                        <Link link="/">Home</Link>
+                        <br/>
+                        <Link link="/page/2">More posts</Link>
+                        <br/>
+                        <Link link="/about-us">About us</Link>
+                    </nav>
+                </HeaderContent>
+            </Header>
             <hr />
-            <main>
+            <Main>
                 <Switch>
                     <List when={data.isArchive}>This is a List</List>
                     <Post when={data.isPost}>This is a Post</Post>
-                    <div when={data.isPage}>This is a Page</div>
+                    <Page when={data.isPage}>This is a Page</Page>
                 </Switch>
-            </main>
+            </Main>
         </>
     )
 }
 
 export default connect(Root)
+
+const Header = styled.header`
+    background-color: #e5edee;
+    border-width: 0 0 8px 0;
+    border-style: solid;
+    border-color: rebeccapurple;
+
+    h1 {
+        color: #4a4a4a;
+    }
+`
+
+const HeaderContent = styled.div`
+    max-width: 800px;
+    padding: 2em 1em;
+    margin: auto;
+`
+const Main = styled.main`
+    max-width: 800px;
+    padding: 1em;
+    margin: auto;
+
+    img {
+        max-width: 100%;
+    }
+    h2 {
+        margin: 0.5em 0;
+    }
+    p {
+        line-height: 1.25em;
+        margin-bottom: 0.75em;
+    }
+    figcaption {
+        color: #828282;
+        font-size: 0.8em;
+        margin-bottom: 1em;
+    }
+`
